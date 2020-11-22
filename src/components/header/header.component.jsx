b237457f858d00efbb159b57eed115a41b2e04b4
Alex <anzyl1986@gmail.com>
2020-11-22 10:50:31 +0300

    finished sign in component, cosmetic changes in other components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,14 +4,14 @@ import {ReactComponent as Logo} from '../../assets/crown.svg';
 import './header.styles.scss';
 
 const Header = () => (
-    <div className="header">
-        <Link className="logo-container" to="/">
-            <Logo className="logo"/>
+    <div className='header'>
+        <Link className='logo-container' to='/'>
+            <Logo className='logo'/>
         </Link>
 
-        <div className="options">
-            <Link className='option' to="/shop">SHOP</Link>
-            <Link className='option' to="/shop">CONTACT</Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>SHOP</Link>
+            <Link className='option' to='/shop'>CONTACT</Link>
         </div>
     </div>
 )

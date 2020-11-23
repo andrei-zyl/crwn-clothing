2f99cbf1c95c05a953508ffa1be7fd6e87672222
Alex <anzyl1986@gmail.com>
2020-11-23 14:50:48 +0300

    finished sing in handleSubmit method, realized the ability to store users in firestore database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -14,10 +14,18 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email: '', password: ''});
-    }
+    
+        const { email, password } = this.state;
+    
+        try {
+          await auth.signInWithEmailAndPassword(email, password);
+          this.setState({ email: '', password: '' });
+        } catch (error) {
+          console.log(error);
+        }
+      };
 
     handleChange = event => {
         const{value, name} = event.target;
@@ -32,20 +40,20 @@ class SignIn extends React.Component{
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput 
-                    name='email'
-                    type='email' 
-                    value={this.state.email} 
-                    handleChange={this.handleChange} 
-                    label='email' 
-                    required/>
+                        name='email'
+                        type='email' 
+                        value={this.state.email} 
+                        handleChange={this.handleChange} 
+                        label='email' 
+                        required/>
 
                     <FormInput 
-                    name='password' 
-                    type='password' 
-                    value={this.state.password} 
-                    handleChange={this.handleChange} 
-                    label='password' 
-                    required/>
+                        name='password' 
+                        type='password' 
+                        value={this.state.password} 
+                        handleChange={this.handleChange} 
+                        label='password' 
+                        required/>
                     
                     <div className='buttons'>
                         <CustomButton type='submit'>Sign in</CustomButton>

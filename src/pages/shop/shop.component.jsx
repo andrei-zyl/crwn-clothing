687ca67272c5763601286e117182c19a3f77bd72
Alex <anzyl1986@gmail.com>
2020-12-01 18:37:30 +0300

    implemented redux saga instead of thunk for redux/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,18 +2,15 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import CollectionPageContainer from '../collection/collection.container';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
 import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
-import {fetchCollectionsStartAsync} from '../../redux/shop/shop.actions';
-import {selectIsCollectionFetching, selectIsCollectionsLoaded} from '../../redux/shop/shop.selectors';
+import {fetchCollectionsStart} from '../../redux/shop/shop.actions';
 
 class ShopPage extends React.Component{
     
     componentDidMount() {
-        const { fetchCollectionsStartAsync } = this.props;
+        const { fetchCollectionsStart } = this.props;
     
-        fetchCollectionsStartAsync();
+        fetchCollectionsStart();
       }
     
     render(){
@@ -28,7 +25,7 @@ class ShopPage extends React.Component{
 } 
 
 const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
   })
 
 export default connect(null, mapDispatchToProps)(ShopPage);
